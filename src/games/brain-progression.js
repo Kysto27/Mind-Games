b04f3b4a096e0925e4968013006acdbb805e46f1
Aleksandr R <kysto27@gmail.com>
2022-11-12 13:31:31 +0300

    brain-progression game version 1

---
+++ src/games/brain-progression.js
@@ -0,0 +1,45 @@
+import brainGame from '../index.js';
+
+const brainProgressionGame = () => {
+  const getQuestionValue = () => {
+    const progression = [];
+    let progressionValue = Math.round(Math.random() * 100);
+    const getProgressionStep = () => {
+      const min = 2;
+      const max = 10;
+      return Math.round(Math.random() * (max - min) + min);
+    };
+    const progressionStep = getProgressionStep();
+    for (let i = 0; i < 10; i += 1) {
+      progression.push(progressionValue);
+      progressionValue += progressionStep;
+    }
+    const progressionHiddenValuePosition = Math.round(Math.random() * 9);
+    progression[progressionHiddenValuePosition] = '..';
+    return progression.join(' ');
+  };
+
+  const getExpectedAnswer = (questionValue) => {
+    const questionValueToArray = questionValue.split(' ');
+    let progressionHiddenValuePosition;
+    let progressionStep;
+    for (let i = 0; i < questionValueToArray.length; i += 1) {
+      if (questionValueToArray[i] === '..') {
+        progressionHiddenValuePosition = i;
+      }
+    }
+    for (let i = 0; i < questionValueToArray.length; i += 1) {
+      if (questionValueToArray[i + 1] - questionValueToArray[i]) {
+        progressionStep = questionValueToArray[i + 1] - questionValueToArray[i];
+      }
+    }
+    const result = (+questionValueToArray[progressionHiddenValuePosition - 1] + +progressionStep);
+    return result.toString();
+  };
+
+  const gameRules = 'What number is missing in the progression?';
+
+  brainGame(getQuestionValue, getExpectedAnswer, gameRules);
+};
+
+export default brainProgressionGame;
