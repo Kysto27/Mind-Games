b1a5dc3391c681b5673d12080a568554a863d5d1
Aleksandr R <kysto27@gmail.com>
2022-11-14 21:13:45 +0300

    fix bug in finction isPrime in prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,15 +5,13 @@ const runPrime = () => {
     const question = Math.round(Math.random() * 100);
 
     const isPrime = (num) => {
-      for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) {
-          return false;
-        }
+      for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+        if (num % i === 0) return false;
       }
-      return true;
+      return num > 1;
     };
 
-    const answer = (isPrime(question) ? 'yes' : 'no');
+    const answer = isPrime(question) ? 'yes' : 'no';
 
     return [question, answer];
   };
