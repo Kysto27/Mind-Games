8c92856c6d0f6c94091cb4ea3eaa49b5e71a23b0
Aleksandr R <kysto27@gmail.com>
2022-11-14 15:14:37 +0300

    delete else construction, return, rename function, optimize code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const brainGame = (getQuestionValue, getExpectedAnswer, gameRules) => {
-  // Приветствие
+const run = (generateRound, gameRules) => {
   console.log('Welcome to the Brain Games!');
-  // Определение имени игрока и вывод приветствия
+
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  // Игровая логика
+
   for (let i = 0; i < 3; i += 1) {
-    // Определение вопроса и правильного ответа
-    const questionValue = getQuestionValue();
-    const expectedAnswer = getExpectedAnswer(questionValue);
-    // Вывод правил ввода ответа
+    const [question, answer] = generateRound();
+
     console.log(gameRules);
-    // Вывод вопроса и запрос ответа от игрока
-    console.log(`Question: ${questionValue}`);
+
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    // Проверка ответа игрока и вывод результата
-    if (userAnswer === expectedAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
-      return console.log(`Let's try again, ${userName}!`);
+
+    if (userAnswer !== answer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+
+    console.log('Correct!');
   }
-  // Вывод поздравления в случае успешного прохождения игры
-  return console.log(`Congratulations, ${userName}!`);
+
+  console.log(`Congratulations, ${userName}!`);
 };
 
-export default brainGame;
+export default run;
