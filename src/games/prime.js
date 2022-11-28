93c8792db6161a3d88c103b053ebdd00764884c2
Aleksandr R <kysto27@gmail.com>
2022-11-28 22:55:42 +0300

    edit function isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,10 +6,14 @@ const minNumber = -100;
 const maxNumber = 100;
 
 const isPrime = (num) => {
-  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+  if (num < 2) {
+    return false;
+  }
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; i += 1) {
     if (num % i === 0) return false;
   }
-  return num > 1;
+  return true;
 };
 
 const generateRound = () => {
