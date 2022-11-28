8905fc246047a22e56cab2e8ddba9a172f98ddf1
Aleksandr R <kysto27@gmail.com>
2022-11-28 22:35:25 +0300

    edit function to find gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,34 +2,21 @@ import run from '../index.js';
 import { generateRandomNumber } from '../utils.js';
 
 const rule = 'Find the greatest common divisor of given numbers.';
-const minNumber = 0;
+const minNumber = 1;
 const maxNumber = 100;
 
-const calculateGcd = (num1, num2) => {
-  let result;
-  let a = num1;
-  let b = num2;
-  if (b > a) { const temp = a; a = b; b = temp; }
-  while (a !== 0 || b !== 0) {
-    if (b === 0) {
-      result = a;
-      break;
-    }
-    a %= b;
-    if (a === 0) {
-      result = b;
-      break;
-    }
-    b %= a;
+const gcd = (x, y) => {
+  if (!y) {
+    return x;
   }
-  return result;
+  return gcd(y, x % y);
 };
 
 const generateRound = () => {
   const number1 = generateRandomNumber(minNumber, maxNumber);
   const number2 = generateRandomNumber(minNumber, maxNumber);
   const question = `${number1} ${number2}`;
-  const answer = calculateGcd(number1, number2).toString();
+  const answer = gcd(number1, number2).toString();
 
   return [question, answer];
 };
